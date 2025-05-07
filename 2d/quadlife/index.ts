14049bc24b82868221052234f8bc53891251901a
Adrien Carpentier <me@adriencarpentier.com>
2025-05-07 13:41:45 +0900

    refactor: better file name and imports

--- 2d/quadlife/index.ts
+++ 2d/quadlife/index.ts
@@ -1,45 +1,64 @@
 import type { Cell } from "../../types/Cell"
-import { ConwayAutomaton } from "../conway/conway"
+import { ConwayAutomaton } from "../conway"
 
-export class ImmigrationAutomaton extends ConwayAutomaton {
+export class QuadLifeAutomaton extends ConwayAutomaton {
 	private aliveColors: Cell[]
 	private aliveColorIds: number[]
 
 	constructor(...args: ConstructorParameters<typeof ConwayAutomaton>) {
 		const [canvasEl, width, height, resolution, _, paletteColors] = args
-		super(canvasEl, width, height, resolution, 3, paletteColors)
-		this.aliveColors = this.colors.slice(1) // Last 2 colors for alive states
+		// Use parent class's color handling, just force 5 colors
+		super(canvasEl, width, height, resolution, 5, paletteColors)
+		this.aliveColors = this.colors.slice(1) // Last 4 colors for alive states
 		this.aliveColorIds = this.aliveColors.map((color) => color.id)
 		this.colorOff = this.colors[0] // First color for dead state
 	}
 
-	private getMostFrequentAliveColor = (
-		aliveNeighbours: Cell[],
-	): Cell | null => {
+	private getQuadLifeColor = (aliveNeighbours: Cell[]): Cell | null => {
 		if (aliveNeighbours.length === 0) return null
-		const occurences = {}
-		let mostFrequentColor = aliveNeighbours[0]
-		let maxFrequency = 1
-		for (const cell of aliveNeighbours) {
-			if (occurences[cell.id] == null) occurences[cell.id] = 1
-			else occurences[cell.id]++
-			if (occurences[cell.id] > maxFrequency) {
-				mostFrequentColor = cell
-				maxFrequency = occurences[cell.id]
-			}
+
+		// Count occurrences of each color
+		const occurrences = new Map<Cell, number>()
+		for (const color of aliveNeighbours) {
+			occurrences.set(color, (occurrences.get(color) || 0) + 1)
 		}
-		return mostFrequentColor
+
+		// Find the maximum occurrence count
+		let maxCount = 0
+		let mostFrequentColors: Cell[] = []
+		occurrences.forEach((count, color) => {
+			if (count > maxCount) {
+				maxCount = count
+				mostFrequentColors = [color]
+			} else if (count === maxCount) {
+				mostFrequentColors.push(color)
+			}
+		})
+
+		if (mostFrequentColors.length === 1) {
+			// If there is only one most frequent color, returns it
+			return mostFrequentColors[0]
+		}
+		if (mostFrequentColors.length === 3) {
+			// Specific QuadLife rule:
+			// Return the only alive color that is not among the most frequent colors
+			return (
+				this.aliveColors.find((color) => !mostFrequentColors.includes(color)) ||
+				null
+			)
+		}
+		return null
 	}
 
 	protected updateState = (): void => {
-		const newState = []
+		const newState: Cell[][] = []
 		for (let y = 0; y < this.rowsCount; ++y) {
 			newState[y] = []
 			for (let x = 0; x < this.colsCount; ++x) {
 				const neighbours = this.getNeighborsColors(x, y)
 
 				// Analyse neighbors info
-				const aliveNeighbours = []
+				const aliveNeighbours: Cell[] = []
 				for (const cell of neighbours) {
 					if (this.aliveColorIds.includes(cell.id)) {
 						aliveNeighbours.push(cell)
@@ -54,7 +73,8 @@ export class ImmigrationAutomaton extends ConwayAutomaton {
 				if (isCellAlive && (isUnderpopulated || isOverpopulated)) {
 					newState[y][x] = this.colorOff
 				} else if (!isCellAlive && isReproduction) {
-					newState[y][x] = this.getMostFrequentAliveColor(aliveNeighbours)
+					newState[y][x] =
+						this.getQuadLifeColor(aliveNeighbours) || this.colorOff
 				} else {
 					newState[y][x] = this.state[y][x]
 				}
